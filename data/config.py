32872a16c2e96e04483ae42cf8139aff78e6a8ef
Barotbek Turxonov <109985351+BarotbekTurxonov@users.noreply.github.com>
2023-02-12 23:43:28 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -5,5 +5,5 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN ="5967594601:AAElrJn0gU4R6WKTaonbCajXuujg8GMKoXo"     # Забираем значение типа str
+BOT_TOKEN ="BOT TOKEN HERE"     # Забираем значение типа str
 ADMINS =   [5235865310] 
